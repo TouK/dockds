5a5a5568340be203023bbcd8bfb453fa20260815
Tomasz Wielga <tw@touk.pl>
2016-10-26 12:09:28 +0200

    optimize imports

--- src/test/java/pl/touk/dockds/DockerizedDataSourceTest.java
+++ src/test/java/pl/touk/dockds/DockerizedDataSourceTest.java
@@ -3,13 +3,7 @@ package pl.touk.dockds;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
